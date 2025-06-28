878fd87e372058f2f3072304084c7d646e3a1216
EHDunagin <dunagin.eleanor225@gmail.com>
2025-06-28 08:03:28 -0400

    Add test for soft delete

--- tests/db_tests.rs
+++ tests/db_tests.rs
@@ -46,8 +46,43 @@ fn test_update_item() {
     add_item(&conn, &item).unwrap();
 
     item.name = "new name".to_string();
-    update_item(&conn, &item);
+    let _ = update_item(&conn, &item);
 
     let fetched = get_item_by_id(&conn, 1).unwrap().unwrap();
     assert_eq!(fetched.name, "new name".to_string());
 }     
+
+#[test]
+fn test_soft_delete() {
+    let conn = Connection::open_in_memory().unwrap();
+    init_db(&conn).unwrap(); // ensure table is created
+
+    let mut item = make_item();
+    add_item(&conn, &item).unwrap();
+
+    item.id = 2;
+    item.name = "item 2".to_string();
+
+    add_item(&conn, &item).unwrap();
+
+    item.id = 3;
+    item.name = "item 3".to_string();
+
+    add_item(&conn, &item).unwrap();
+
+    let _ = soft_delete_item(&conn, 2);
+
+    let all_items = get_all_items(&conn).unwrap();
+    assert_eq!(all_items.len(), 2);
+
+    let ids: Vec<i32> = all_items.iter().map(|item| item.id).collect();
+    assert!(ids.contains(&1), "Missing item with id 1");
+    assert!(!ids.contains(&2), "Contains deleted item with id 2");
+    assert!(ids.contains(&3), "Missing item with id 3");
+
+}
+
+
+
+
+
