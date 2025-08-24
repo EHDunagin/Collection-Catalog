c93e399b734eb0cb08aa3d4146ca048df16fd0e2
EHDunagin <dunagin.eleanor225@gmail.com>
2025-08-24 10:42:27 -0400

    Work on Tauri setiup. Out-of-box create-tauri app now working. Next steps begin adding logic to use core functions to build application ui

---
+++ collection-catalog-ui/src-tauri/src/main.rs
@@ -0,0 +1,31 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::fs;
+use std::path::Path;
+
+use collection_catalog_core::{init_db, get_all_items, Item};
+use rusqlite::Connection;
+
+#[tauri::command]
+fn list_items() -> Result<Vec<Item>, String> {
+    // Ensure data dir exists
+    let data_dir = Path::new("data");
+    if !data_dir.exists() {
+        fs::create_dir_all(data_dir).map_err(|e| e.to_string())?;
+    }
+
+    let conn = Connection::open("data/catalog.db").map_err(|e| e.to_string())?;
+    init_db(&conn).map_err(|e| e.to_string())?;
+    
+    // Return the items
+    let items =  get_all_items(&conn).map_err(|e| e.to_string())?;
+    Ok(items)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![list_items])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri applicateion");
+}
