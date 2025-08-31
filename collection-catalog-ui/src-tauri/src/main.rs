3faf6483110a63fa6efebfbc4960274e50c2efe4
EHDunagin <dunagin.eleanor225@gmail.com>
2025-08-31 10:45:15 -0400

    Implement minimal connection to DB and core functionality. Added button to frontent to list items from DB using get_all_items from collection catalog core. This demonstrated POC that it will successfully use code from core. Had to update path to db to parent parent of parent. Previously was creating a new data directory in collection-catalog-ui/src-tauri. Had to modify js to work with this as well.

--- collection-catalog-ui/src-tauri/src/main.rs
+++ collection-catalog-ui/src-tauri/src/main.rs
@@ -3,23 +3,25 @@
 
 use std::fs;
 use std::path::Path;
+use std::sync::Mutex;
 
-use collection_catalog_core::{init_db, get_all_items, Item};
+use collection_catalog_core::{ init_db, get_all_items, Item };
 use rusqlite::Connection;
+use tauri::State;
+
+// Shared state wrapper
+struct DbState(Mutex<Connection>);
+
 
 #[tauri::command]
-fn list_items() -> Result<Vec<Item>, String> {
-    // Ensure data dir exists
-    let data_dir = Path::new("data");
-    if !data_dir.exists() {
-        fs::create_dir_all(data_dir).map_err(|e| e.to_string())?;
-    }
+fn list_items(db: State<DbState>) -> Result<Vec<Item>, String> {
 
-    let conn = Connection::open("data/catalog.db").map_err(|e| e.to_string())?;
-    init_db(&conn).map_err(|e| e.to_string())?;
+    // println!("list_items called from frontend");
+    let conn = db.0.lock().map_err(|e| e.to_string())?;
     
     // Return the items
-    let items =  get_all_items(&conn).map_err(|e| e.to_string())?;
+    let items =  get_all_items(&*conn).map_err(|e| e.to_string())?;
+    // println!("list_items returning {} items", items.len());
     Ok(items)
 }
 
@@ -29,8 +31,19 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // Ensure data dir exists
+    let data_dir = Path::new("../../data");
+    if !data_dir.exists() {
+        fs::create_dir_all(data_dir).expect("Failed to create database and no existing database found");
+    }
+
+    // Initialize DB connection
+    let conn = Connection::open("../../data/catalog.db").expect("failed to open db");
+    init_db(&conn).expect("failed to init db");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(DbState(Mutex::new(conn))) // add DB to app state
+        .invoke_handler(tauri::generate_handler![list_items])
         .run(tauri::generate_context!())
-        .expect("error while running tauri applicateion");
+        .expect("error while running tauri application");
 }
--- collection-catalog-ui/src/main.js
+++ collection-catalog-ui/src/main.js
@@ -1,15 +1,33 @@
 const { invoke } = window.__TAURI__.core;
 
-const greetButton = document.getElementById("greet-button");
-const greetMsg = document.getElementById("greet-msg");
-
-greetButton.addEventListener("click", async () => {
-	try {
-		// Call the Rust command
-		const response = await invoke("greet", { name: "Eleanor" });
-		greetMsg.textContent = response;
-	} catch (error) {
-		console.error("Error calling Rust:", error);
-	}
+document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("list-button").addEventListener("click", async () => {
+		const items = await invoke("list_items");
+		console.log("loaded items:", items);
+		// document.getElementById("greet-msg").innerText = items.join(", ");
+		const item_list = document.getElementById("items-list");
+		item_list.innerHTML = "";
+		items.forEach(item => {
+			const li = document.createElement("li");
+			li.textContent = `${item.name} - ${item.description}`;
+			item_list.appendChild(li);
+		});
+	});
 });
+
+
+
+
+// const greetButton = document.getElementById("greet-button");
+// const greetMsg = document.getElementById("greet-msg");
+
+// greetButton.addEventListener("click", async () => {
+// 	try {
+// 		// Call the Rust command
+// 		const response = await invoke("greet", { name: "Eleanor" });
+// 		greetMsg.textContent = response;
+// 	} catch (error) {
+// 		console.error("Error calling Rust:", error);
+// 	}
+// });
 
