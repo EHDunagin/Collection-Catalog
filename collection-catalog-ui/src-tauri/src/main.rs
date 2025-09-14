17d1b4c95ea559c7cc128bbba9719cb527b3a6d1
EHDunagin <dunagin.eleanor225@gmail.com>
2025-09-14 09:59:30 -0400

    Implement filtering. Added form for ItemFilter fields and logic to retrieve list of filtered items. Redirects to a new page with a list of items based on the applied filters. Fixed bug in add logic to allow for null values in 'working' field.

--- collection-catalog-ui/src-tauri/src/main.rs
+++ collection-catalog-ui/src-tauri/src/main.rs
@@ -4,8 +4,9 @@
 use std::fs;
 use std::path::Path;
 use std::sync::Mutex;
+use std::collections::HashMap;
 
-use collection_catalog_core::{ init_db, get_all_items, add_item, Item };
+use collection_catalog_core::{ init_db, get_all_items, get_filtered_items, get_item_by_id, add_item, update_item_fields, soft_delete_item, export_to_csv, Item, ItemFilter };
 use rusqlite::Connection;
 use tauri::State;
 
@@ -19,14 +20,42 @@ fn list_items(db: State<DbState>) -> Result<Vec<Item>, String> {
     Ok(get_all_items(&*conn).map_err(|e| e.to_string())?)
 }
 
-// TODO figure out how to use this in such a way as to take info from the frontend, create an Item,
-// and pass it into the function
 #[tauri::command]
 fn new_item(db: State<DbState>, item: Item) -> Result<(), String> {
     let conn = db.0.lock().map_err(|e| e.to_string())?;
     item.validate().map_err(|errs| errs.join(", "))?;
     Ok(add_item(&conn, &item).map_err(|e| e.to_string())?)
 }
+
+#[tauri::command]
+fn filter_items(db: State<DbState>, filter: ItemFilter) -> Result<Vec<Item>, String> {
+    let conn = db.0.lock().map_err(|e| e.to_string())?;
+    Ok(get_filtered_items(&*conn, filter).map_err(|e| e.to_string())?)
+}
+
+#[tauri::command]
+fn get_item(db: State<DbState>, id: i32) -> Result<Option<Item>, String> {
+    let conn = db.0.lock().map_err(|e| e.to_string())?;
+    get_item_by_id(&*conn, id).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+fn update_item(db: State<DbState>, id: i32, updates: HashMap<String, String>) -> Result<(), String> {
+    let conn = db.0.lock().map_err(|e| e.to_string())?;
+
+    let owned: HashMap<String, String> = updates;
+    let borrowed: HashMap<&str, String> = 
+        owned.iter().map(|(k, v)| (k.as_str(), v.clone())).collect();
+    update_item_fields(&conn, id, borrowed)
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+fn delete_item(db: State<DbState>, id: i32) -> Result<(), String> {
+    let conn = db.0.lock().map_err(|e| e.to_string())?;
+    soft_delete_item(&*conn, id).map_err(|e| e.to_string())
+}
+
 
 
 fn main() {
@@ -42,7 +71,15 @@ fn main() {
 
     tauri::Builder::default()
         .manage(DbState(Mutex::new(conn))) // add DB to app state
-        .invoke_handler(tauri::generate_handler![list_items, new_item])
+        .invoke_handler(tauri::generate_handler![
+            list_items, 
+            new_item,
+            filter_items,
+            get_item,
+            update_item,
+            delete_item
+
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
