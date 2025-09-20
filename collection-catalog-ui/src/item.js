598b64bb31077806a2c8db4a017dcd7ffdae0dba
EHDunagin <dunagin.eleanor225@gmail.com>
2025-09-20 07:45:03 -0400

    Create item.html page and modify add-item. Created item.html page to show details of an individual item. Modified add-item page so that it doesn't always pass a boolean for working condition field. It was defaulting to false if the box was unchecked but shoud be optional for items where it is not applicable. Minor cleanup on filter.html and filter.js to remove commented out code that isn't needed

---
+++ collection-catalog-ui/src/item.js
@@ -0,0 +1,14 @@
+<body>
+  <h2>Item Details</h2>
+
+  <div id="item-details">
+    Loading item...
+  </div>
+
+  <br>
+  <button id="update-button">Update Item</button>
+  <button id="delete-button">Delete Item</button>
+
+  <script src="item.js"></script>
+</body>
+</html>
